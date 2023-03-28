7afc2976f7676b762e33f34cf311315c07476cd7
udaykashyap <udaykashyap.hjp@gmail.com>
2023-03-28 21:24:23 +0530

    folder structure done

--- frontend/src/Pages/SingleProductPage/SingleProduct.jsx
+++ frontend/src/Pages/SingleProductPage/SingleProduct.jsx
@@ -5,4 +5,3 @@ const SingleProduct = () => {
 };
 
 export default SingleProduct;
-SingleProduct;
---
+++ frontend/src/Routes/AllRoutes.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Admin from "../Pages/AdminPage/Admin";
+import Checkout from "../Pages/CheckoutPage/Checkout";
+import Home from "../Pages/HomePage/Home";
+import Login from "../Pages/LoginPage/Login";
+import Product from "../Pages/ProductPage/Product";
+import Signup from "../Pages/SignupPage/Signup";
+import SingleProduct from "../Pages/SingleProductPage/SingleProduct";
+
+const AllRoutes = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/product" element={<Product />} />
+        <Route path="/product/:ID" element={<SingleProduct />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/admin" element={<Admin />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="*" element={<h1>Sorry Not Found</h1>} />
+      </Routes>
+    </div>
+  );
+};
+
+export default AllRoutes;

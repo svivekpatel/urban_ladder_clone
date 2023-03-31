411e7a7c0f3c972d2d307bace49fc26d32f45e81
udaykashyap <udaykashyap.hjp@gmail.com>
2023-04-01 01:08:43 +0530

    working on backend

---
+++ backend/Model/user.model.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+
+const userSchema = mongoose.Schema({
+    Name: { type: String, required: true },
+    email: { type: String, unique: true, required: true },
+    password: { type: String, required: true }
+}, {
+    versionKey: false
+})
+
+const UserModel = mongoose.model('user', userSchema)
+
+module.exports = UserModel

67ae84cc91cf1f6377e2af1f1230ae3a64efe8a7
Vivek Patel <vp133202@gmail.com>
2023-04-01 02:34:51 +0530

    cart page added

---
+++ frontend/src/Pages/AddressPage/Cart.jsx
@@ -0,0 +1,20 @@
+import { Box, Grid } from '@chakra-ui/layout'
+import React from 'react'
+import CartSummary from '../../Components/UserSide/Address/CartSummary'
+import CartProducts from '../../Components/UserSide/Address/CartProducts'
+import Navigation from '../../Components/UserSide/Address/Navigation'
+
+
+const Cart = () => {
+  return (
+    <Box w={{ base: "98%", md: "90%", lg: "75%" }} margin={"auto"} pt={"150px"}>
+        <Navigation/>
+        <Grid templateColumns={{ sm: "100%", md: "100%", lg: "60% 40%" }} gap={"10px"}>
+          <CartProducts />
+          <CartSummary />
+        </Grid>
+    </Box>
+  )
+}
+
+export default Cart;

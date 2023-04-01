d20753fe55920c2e09ac74c3125512441eebc389
udaykashyap <udaykashyap.hjp@gmail.com>
2023-04-01 23:15:03 +0530

    backend done

--- backend/Model/user.model.js
+++ backend/Model/user.model.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose')
 const userSchema = mongoose.Schema({
     Name: { type: String, required: true },
     email: { type: String, unique: true, required: true },
-    password: { type: String, required: true }
+    password: { type: String, required: true },
+    contact: Number
 }, {
     versionKey: false
 })

7e063af63946e4379906ffb1a6b99d153c5bbf3a
Vivek Patel <vp133202@gmail.com>
2023-04-03 01:01:52 +0530

    cart update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,13 +3,9 @@ import React from "react";
 import Footer from "./Components/UserSide/Footer/Footer";
 import Navbar from "./Components/UserSide/Navbar/Navbar";
 import AllRoutes from "./Routes/AllRoutes";
+import Cart from "./Pages/AddressPage/Cart";
 
-import Footer from './Components/UserSide/Footer/Footer';
-import Navbar from './Components/UserSide/Navbar/Navbar';
-import Address from './Pages/AddressPage/Address';
-import Cart from './Pages/AddressPage/Cart';
-import Payment from './Pages/AddressPage/Payment';
-import AllRoutes from './Routes/AllRoutes';
+
 
 function App() {
   const [adminLogin, setAdminLogin] = React.useState(false);
@@ -19,14 +15,9 @@ function App() {
       {/* <Product/> */}
       {adminLogin ? "" : <Navbar setAdminLogin={setAdminLogin} />}
       <AllRoutes />
+      <Cart/>
 
       {adminLogin ? "" : <Footer />}
-      <Navbar />
-      {/* <AllRoutes /> */}
-      <Cart/>
-      <Address/>
-      <Payment/>
-      <Footer />
     </div >
   );
 }
